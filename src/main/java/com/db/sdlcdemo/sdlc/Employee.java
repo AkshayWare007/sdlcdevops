177ce4e5542301a8e96d10a662ab2734a0ff8060
Akshay <akshayware1207@gmail.com>
2020-09-21 16:57:00 +0530

    created demo sdlc project

---
+++ src/main/java/com/db/sdlcdemo/sdlc/Employee.java
@@ -0,0 +1,31 @@
+package com.db.sdlcdemo.sdlc;
+
+/**
+ * Created by Akshay on 21-09-2020.
+ */
+public class Employee {
+
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
